637358b5665ebe2bc9b663ed099edd630d24ede3
Uniboy-ROS <uniboy@virtuosorobotics.com>
2020-01-06 17:57:30 +0800

    serial_communication

--- serial_communication/openlight/openlight.ino
+++ serial_communication/openlight/openlight.ino
@@ -1,22 +1,101 @@
-#define LED 13
+int brightness1 ;                   //亮度
+int fadeAmount1 = 25;               //亮度增幅值
+int brightness2 ;
+int fadeAmount2 = 25;
+
 String str;
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(10, OUTPUT);  //green
+  pinMode(11, OUTPUT);  //blue
   Serial.begin(115200);
 }
 
 void loop() {
+  analogWrite(10, brightness1);
+  analogWrite(11, brightness2);
   if (Serial.available()) {
     // 讀取傳入的字串直到"\n"結尾
     str = Serial.readStringUntil('\n');
 
-    if (str == "LED_ON") {           // 若字串值是 "LED_ON" 開燈
-        digitalWrite(LED, HIGH);     // 開燈
-        Serial.println("LED is ON"); // 回應訊息給電腦
-    } else if (str == "LED_OFF") {
-        digitalWrite(LED, LOW);
-        Serial.println("LED is OFF");
+    if (str == "GLED_UP") {                                     // 若字串值是 "LED_ON" 開燈
+      brightness1 = brightness1 + fadeAmount1;                  //增加亮度
+      if (brightness1 > 255) {                                  //設保護值，當亮度大於255時，就直接等於255，以避免亮度值爆表造成LED有不正常閃光
+        brightness1 = 255;
+      }
+      //Serial.println(brightness1);                            //序列埠印出亮度值
+      //digitalWrite(10, HIGH);     // 開燈
+      Serial.println("GreenLED brightness is ");
+      Serial.println(brightness1);
+    }
+    else if (str == "GLED_DOWN") {
+      brightness1 = brightness1 - fadeAmount1;
+      if (brightness1 < 0) {                                    //設保護值，當亮度小於0時，就直接等於0，以避免亮度值爆表造成LED有不正常閃光
+        brightness1 = 0;
+      }
+      //digitalWrite(10, LOW);
+      Serial.println("GreenLED brightness is ");
+      Serial.println(brightness1);
+    }
+    else if (str == "GLED_ON") {                                // 若字串值是 "LED_ON" 開燈
+      brightness1 = 255;                                        // 開燈
+      Serial.println("GreenLED is ON");                       // 回應訊息給電腦
+    }
+    else if (str == "GLED_OFF") {
+      brightness1 = 0;
+      Serial.println("GreenLED is OFF");
+    }
+
+
+    else if (str == "BLED_UP") {
+      brightness2 = brightness2 + fadeAmount2;
+
+      if (brightness2 > 255) {
+        brightness2 = 255;
+      }
+      //digitalWrite(11, HIGH);
+      Serial.println("BlueLED brightness is ");
+      Serial.println(brightness2);
+    }
+    else if (str == "BLED_DOWN") {
+      brightness2 = brightness2 - fadeAmount2;
+      if (brightness2 < 0) {
+        brightness2 = 0;
+      }
+      //digitalWrite(11, LOW);
+      Serial.println("BlueLED brightness is ");
+      Serial.println(brightness2);
+    }
+    else if (str == "BLED_ON") {                                // 若字串值是 "LED_ON" 開燈
+      brightness2 = 255;                                         // 開燈
+      Serial.println("BlueLED is ON");                       // 回應訊息給電腦
+    }
+    else if (str == "BLED_OFF") {
+      brightness2 = 0;
+      Serial.println("BlueLED is OFF");
+    }
+
+    else if (str == "flicker") {
+
+      digitalWrite(10, HIGH);                               // 點亮LED
+      digitalWrite(11, HIGH);
+      delay(300);                                          // 延時1000ms(即1秒)
+      digitalWrite(10, LOW);                                // 熄滅LED
+      digitalWrite(11, LOW);
+      delay(300);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      delay(300);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      delay(300);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      delay(300);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+
+      Serial.println("flash");
     }
   }
 }
--- serial_communication/serial_communication.py
+++ serial_communication/serial_communication.py
@@ -9,16 +9,44 @@ ser = serial.Serial(COM_PORT, BAUD_RATES)
 try:
     while True:
         # 接收用戶的輸入值並轉成小寫
-        choice = input('按1開燈、按2關燈、按e關閉程式  ').lower()
+        choice = input('按1.2加減綠、按4.5加減藍、3綠開、6藍開、7綠關、8藍關、e=close  ').lower()
 
         if choice == '1':
-            print('傳送開燈指令')
-            ser.write(b'LED_ON\n')  # 訊息必須是位元組類型
+            print('傳送綠燈指令')
+            ser.write(b'GLED_UP\n')  # 訊息必須是位元組類型
             sleep(0.5)              # 暫停0.5秒，再執行底下接收回應訊息的迴圈
         elif choice == '2':
-            print('傳送關燈指令')
-            ser.write(b'LED_OFF\n')
+            print('傳送綠燈指令')
+            ser.write(b'GLED_DOWN\n')
             sleep(0.5)
+        elif choice == '4':
+            print('傳送藍燈指令')
+            ser.write(b'BLED_UP\n')
+            sleep(0.5)
+        elif choice == '5':
+            print('傳送藍燈指令')
+            ser.write(b'BLED_DOWN\n')
+            sleep(0.5)
+        elif choice == '3':
+            print('傳送綠燈指令')
+            ser.write(b'GLED_ON\n')
+            sleep(0.5)
+        elif choice == '6':
+            print('傳送藍燈指令')
+            ser.write(b'BLED_ON\n')
+            sleep(0.5) 
+        elif choice == '7':
+            print('傳送綠燈指令')
+            ser.write(b'GLED_OFF\n')
+            sleep(0.5)
+        elif choice == '8':
+            print('傳送藍燈指令')
+            ser.write(b'BLED_OFF\n')
+            sleep(0.5)    
+        elif choice == 'f':
+            print('閃爍一次')
+            ser.write(b'flicker\n')
+            sleep(1.7) 
         elif choice == 'e':
             ser.close()
             print('再見！')
